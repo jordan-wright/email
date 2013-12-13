0a6b29524fbca4f8c28dd7bfe931b6cb54bc8378
Jordan Wright <jmwright798@gmail.com>
2013-12-13 00:16:39 -0600

    Initial commit - about 15 minutes work of work.

---
+++ email.go
@@ -0,0 +1,29 @@
+//Email is designed to be a package providing an "email interface for humans."
+//Designed to be robust and flexible, the email package will make sending email easy without getting in the way.
+package email
+
+import (
+	"net/mail"
+)
+
+//Email is the type used for email messages
+type Email struct {
+	From        string
+	To          []string
+	Bcc         []string
+	Cc          []string
+	Subject     []string
+	Text        []byte //Plaintext message (optional)
+	Html        []byte //Html message (optional)
+	Headers     []mail.Header
+	Attachments []Attachment //Might be a map soon - stay tuned.
+}
+
+//Send an email using the given host and SMTP auth (optional)
+func (e Email) Send() {
+
+}
+
+type Attachment struct {
+	Filename string
+}
